210ce7f7838c04451c664f19bf6c3b4762e88690
Matiass5175 <matheus.mota@quanta.org.br>
2024-08-23 15:25:33 -0300

    aaa

--- docx_page/run_intro_page.py
+++ docx_page/run_intro_page.py
@@ -35,7 +35,7 @@ def run_intro_page():
 
         #gráfico no Streamlit
         return st.plotly_chart(fig)
-    st.title("Apresentação: ")
+    st.title("Introdução: ")
     st.subheader('O que é a programação matemática?')
     st.write('Uma das principais áreas de pesquisa na matemática aplicada é a programação matemática. O objeto de estudo dessa área é a otimização de uma função, denominada função objetivo, que pode ser maximizada ou minimizada, de modo que tal função é sujeita a restrições que são representadas por desigualdades.')
     st.subheader('Quais são as principais áreas de estudo da programação matemática na graduação?')
